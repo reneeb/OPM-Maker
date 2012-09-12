3c5ace1ebf20d819afad1865f8e144524db6c9a4
reneeb <github@renee-baecker.de>
2012-09-12 17:11:40 +0200

    add command to list dependencies

---
+++ lib/OTRS/OPM/Maker/Command/dependencies.pm
@@ -0,0 +1,54 @@
+package OTRS::OPM::Maker::Command::dependencies;
+
+use strict;
+use warnings;
+
+use OTRS::OPM::Maker -command;
+
+sub abstract {
+    return "list dependencies for OTRS packages";
+}
+
+sub usage_desc {
+    return "opmbuild dependencies <path_to_opm>";
+}
+
+sub validate_args {
+    my ($self, $opt, $args) = @_;
+    
+    $self->usage_error( 'need path to .sopm' ) if
+        !$args ||
+        !$args->[0] ||
+        !$args->[0] =~ /\.sopm\z/ ||
+        !-f $args->[0];
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+    
+    my $file = $args->[0];
+    my $parser = XML::LibXML->new;
+    my $tree   = $parser->parse_file( $file );
+        
+    my $root_elem = $tree->getDocumentElement;
+    
+    # retrieve file information
+    my @package_req = $root_elem->findnodes( 'PackageRequired' );
+    my @modules_req = $root_elem->findnodes( 'ModuleRequired' );
+    
+    my %labels = (
+        PackageRequired => 'OTRS add on',
+        ModuleRequired  => 'CPAN module',
+    );
+        
+    DEP:
+    for my $dependency ( @package_req, @modules_req ) {
+        my $type    = $dependency->nodeName;
+        my $version = $dependency->findvalue( '@Version' );
+        my $name    = $dependency->textContent;
+        
+        print "$name - $version (" . $labels{$type} . ")\n";
+    }
+}
+
+1;
