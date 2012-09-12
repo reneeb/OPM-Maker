b0d941621de1e494fa6508ae0e43dd28ccebcc02
reneeb <github@renee-baecker.de>
2012-09-12 17:19:34 +0200

    work on remote_install command (define options and some of the needed subroutines)

--- lib/OTRS/OPM/Maker/Command/remote_install.pm
+++ lib/OTRS/OPM/Maker/Command/remote_install.pm
@@ -5,4 +5,35 @@ use warnings;
 
 use OTRS::OPM::Maker -command;
 
+sub abstract {
+    return "install package in OTRS instance";
+}
+
+sub usage_desc {
+    return "opmbuild remote_install --host <host> --token <token> --user <user> [--test [--format <format>]] <path_to_opm>";
+}
+
+sub opt_spec {
+    return (
+        [ "host=s", "hostname of remote OTRS instance" ],
+        [ "token=s", "API token for remote OTRS instance" ],
+        [ "user=s", "username for remote OTRS instance" ],
+        [ "test", "run tests on remote OTRS instance" ],
+        [ "format", "format of test output (TAP or JUnit)" ],
+    );
+}
+
+sub validate_args {
+    my ($self, $opt, $args) = @_;
+    
+    $self->usage_error( 'need path to .opm' ) if
+        !$args ||
+        !$args->[0] ||
+        !$args->[0] =~ /\.opm\z/ ||
+        !-f $args->[0];
+}
+
+sub execute {
+}
+
 1;
