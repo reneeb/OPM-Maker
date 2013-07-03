39165978e682ff292e0e737cb1bb41aca2371ab2
reneeb <github@renee-baecker.de>
2013-07-03 10:47:45 +0200

    fix xsd for .sopm

--- lib/OTRS/OPM/Maker.pm
+++ lib/OTRS/OPM/Maker.pm
@@ -4,7 +4,7 @@ use App::Cmd::Setup -app;
 
 # ABSTRACT: Module/App to build and test OTRS packages
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 =head1 DESCRIPTION
 
@@ -30,14 +30,6 @@ List all CPAN- and OTRS- dependencies of your package
 
 Create the OPM file
 
-=item * devlink
-
-When you develop the package you might not want to build new version of your package and install it via the package manager. This command creates symlinks for each file of your package.
-
-=item * devunlink
-
-Remove the symlinks created with C<devlink>.
-
 =item * index
 
 build an index file for an OPM repository.
@@ -55,16 +47,6 @@ Install the OPM file on any OTRS instance. That remote OTRS instance need an OTR
 =item * dbtest
 
 Check if the C<DatabaseInstall> and C<DatabaseUninstall> sections in your .sopm files are valid. And it checks for SQL keywords.
-
-=back
-
-Ideas:
-
-=over 4
-
-=item * devdb
-
-Do the DB changes needed for your package
 
 =back
 
