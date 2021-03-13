8ccf3ec266e1d3c9b7bd2635047a73493d75f9a7
reneeb <info@perl-services.de>
2021-03-13 11:29:26 +0100

    Revert "cleanup"
    This reverts commit 05e22e5b3712fb8621cf92138a0cf7fc5cfaf068.

---
+++ lib/OPM/Maker/Command/dbtest.pm
@@ -0,0 +1,33 @@
+package OPM::Maker::Command::dbtest;
+
+# ABSTRACT: Test db definitions in .sopm files
+
+use strict;
+use warnings;
+
+use OPM::Maker -command;
+
+sub abstract {
+    return "Check if DatabaseInstall and DatabaseUninstall sections in the .sopm are correct";
+}
+
+sub usage_desc {
+    return "opmbuild dbtest <path_to_sopm>";
+}
+
+sub validate_args {
+    my ($self, $opt, $args) = @_;
+
+    $self->usage_error( 'need path to .sopm' ) if
+        !$args or
+        'ARRAY' ne ref $args or
+        !defined $args->[0] or
+        $args->[0] !~ /\.sopm\z/ or
+        !-f $args->[0];
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+}
+
+1;
